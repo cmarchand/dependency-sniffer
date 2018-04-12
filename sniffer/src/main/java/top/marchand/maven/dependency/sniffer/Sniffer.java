3d73b224823043d484d5da8aa7af39284df31960
Christophe Marchand <christophe@marchand.top>
2018-04-12 18:00:05 +0200

    create

---
+++ sniffer/src/main/java/top/marchand/maven/dependency/sniffer/Sniffer.java
@@ -0,0 +1,20 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License, 
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package top.marchand.maven.dependency.sniffer;
+
+/**
+ *
+ * @author cmarchand
+ */
+public class Sniffer {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public static final String SYNTAX = "java top.marchand.maven.dependency.sniffer.Sniffer -c config <fileName>";
+    
+}
